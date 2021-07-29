1a8a0a51efe68a1bc2fd97634e7d51d295e94aa8
unknown <threepinidea@gmaill.com>
2021-07-30 01:39:14 +0800

    final

--- try-password.py
+++ try-password.py
@@ -14,10 +14,13 @@
 password = 'a123456'
 i = 3
 while i > 0:
+    i -= 1
     pwd = input('請輸入密碼:')
     if pwd == password:
         print('登入成功')
         break
     else:
-        i -= 1
-        print('密碼錯誤! 還剩: ', i, '機會')
+        if i > 0:
+            print('還剩:', i, '機會')
+        else:
+            print('密碼多次錯誤!','\n請聯絡客服')
